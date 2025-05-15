98d0b19adf65dc4c70188ca7ed4a96dc18cde5c2
abdozcan <abdozcann@gmail.com>
2025-05-15 14:38:11 +0300

    Added Kotlin extension properties for months to Int

---
+++ src/main/kotlin/MonthExt.kt
@@ -0,0 +1,14 @@
+package org.abdozcan
+
+val Int.Jan get() = "January, $this"
+val Int.Feb get() = "February, $this"
+val Int.Mar get() = "March, $this"
+val Int.Apr get() = "April, $this"
+val Int.May get() = "May, $this"
+val Int.Jun get() = "June, $this"
+val Int.Jul get() = "July, $this"
+val Int.Aug get() = "August, $this"
+val Int.Sep get() = "September, $this"
+val Int.Oct get() = "October, $this"
+val Int.Nov get() = "November, $this"
+val Int.Dec get() = "December, $this"
